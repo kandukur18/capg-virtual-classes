018dbfb90e8935952c2ac6a6efa2c4896202096b
vennela <vennela@USER>
2020-04-07 11:50:40 -0700

    1

--- capgemini-bvrit-master/demo-spring-boot-simple/src/main/java/com/capg/demo/exceptions/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.capg.demo.exceptions;
-
-public class UserNotFoundException extends RuntimeException {
-public UserNotFoundException(String message) {
-	// TODO Auto-generated constructor stub
-	super(message);
-}
-}
